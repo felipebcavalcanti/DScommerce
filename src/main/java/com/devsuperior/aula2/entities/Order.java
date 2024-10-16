72e323e88feccef9183552e3c9a51bbb01a459bf
Felipe Cavalcanti <fb.cavalcanti1@gmail.com>
2024-10-16 14:42:11 -0300

    "add: Payments e relacionamentos"

--- src/main/java/com/devsuperior/aula2/entities/Order.java
+++ src/main/java/com/devsuperior/aula2/entities/Order.java
@@ -18,5 +18,40 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payments payments;
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payments getPayments() {
+        return payments;
+    }
+
+    public void setPayments(Payments payments) {
+        this.payments = payments;
+    }
+
 
 }
